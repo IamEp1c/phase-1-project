d01d51cb1e47694db61fc845dc1e39262e133c99
Waqas Anjum <Qanjum96@gmail.com>
2022-04-21 16:23:55 -0400

    made a random anime generator and a list format

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ document.addEventListener('DOMContentLoaded', (e) => {
         fetch(`https://anime-facts-rest-api.herokuapp.com/api/v1/${animeName}`)
             .then(resp => resp.json())
             .then(data => {
-                console.log(data)
-                console.log(data.data)
+                // console.log(data)
                 renderFact(data.data)
 
             })
     } // end of userSelection
     // grabs the drop down menu and puts the names in the function userSelection and grabs each name of the anime
     document.querySelector('.dropdown-content').addEventListener('click', (e) => {
-        console.log(e.target.id)
+        // console.log(e.target.id)
         userSelection(e.target.id);
-        renderFact()
+
+        
     })
 
 
     const renderFact = (data) => {
-        data.forEach(anime => {
+        data.forEach(anime  => {
             console.log(anime.fact)
 
             const ul = document.querySelector('#ol')
@@ -34,33 +34,57 @@ document.addEventListener('DOMContentLoaded', (e) => {
             ul.append(li)
         })
     }
+
     document.querySelector('#new-comment')
     const form = document.querySelector('#comments-form')
-    form.addEventListener('submit', (e) => addCom(e))
-    const addCom = (e) => {
-        e.preventDefault();
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
         const ul = document.querySelector('#ul')
         const li = document.createElement('li')
         const newComment = document.querySelector('#comment')
         li.textContent = newComment.value
-        ul.append(li)
-
-        
-        
-      
-
-    }
+        ul.append(li)        
+    })
 
 
 
+const randomFactSelector = (data) => {
+    const index =  Math.floor(Math.random() * data.data.length);
+    return data.data[index].fact
 
-    // 
+}
 
 
 
 
+// click event for Random Generator
+const startButton = document.querySelector('.start-button')
+const quotesContainer = document.querySelector('#quotesContainer')
 
-    // 
+
+
+startButton.addEventListener('click', (e) =>{
+    e.preventDefault();
+    
+    const animeNames = ['attack_on_titan', 'jujutsu_kaisen', 'demon_slayer']
+    const index =  Math.floor(Math.random() * animeNames.length);
+    const animeName = animeNames[index]
+    const p = document.createElement('p')
+    fetch(`https://anime-facts-rest-api.herokuapp.com/api/v1/${animeName}`)
+    .then(resp => resp.json())
+    .then(data => {
+        p.textContent = randomFactSelector(data)
+    })
+
+    quotesContainer.innerHTML = ''
+    quotesContainer.append(p)
+    
+})
+
+
+
+
+ 
 }) // end of domContentLoaded
 
 
